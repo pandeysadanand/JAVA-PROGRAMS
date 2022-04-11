d614ef287f4cbf1643c1a8b54398d92a18ca1b9b
sadanand pandey <pandeysadanad75@gmail.com>
2022-04-11 13:31:36 +0530

    Added: Printing message on console add

---
+++ message_print.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+/*
+ * Printing Name on Console
+ * @author : Sadanand Pandey
+ */
+public class message_print {
+
+	public static void main(String[] args) {
+		System.out.println("Hello"); 
+
+	}
+
+}
