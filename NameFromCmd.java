d0ea552dddbe7818ccabd97ee7a4977e86b3883f
sadanand pandey <pandeysadanad75@gmail.com>
2022-04-11 13:13:09 +0530

    Added: Taking input from command line add

---
+++ NameFromCmd.java
@@ -0,0 +1,15 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class NameFromCmd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Your name");
+		String s = sc.nextLine();
+		System.out.println(s);
+	}
+
+}
