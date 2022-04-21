d4888463dd924d0e83f437706677cc7bd6bf10ac
sadanand pandey <pandeysadanad75@gmail.com>
2022-04-21 19:23:23 +0530

    Added: Printing largest elements from array

---
+++ LargestElement_array.java
@@ -0,0 +1,19 @@
+package com.bridgelabz;
+
+/*
+ *  This program is printing largest element from an array
+ *  @author : Sadanand Pandey
+ */
+public class LargestElement_array {
+	public static void main(String[] args) {
+
+		int[] arr = new int[] { 25, 11, 97, 75, 56 };
+		int max = arr[0];
+		for (int i = 0; i < arr.length; i++) {
+
+			if (arr[i] > max)
+				max = arr[i];
+		}
+		System.out.println("Largest element present in given array: " + max);
+	}
+}
